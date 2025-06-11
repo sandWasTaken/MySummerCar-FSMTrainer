f3cdbec062ffe9c2ba5b26c5399c265ff039d732
sand <69005170+sandWasTaken@users.noreply.github.com>
2025-06-11 02:33:38 -0500

    Add jump-to-state, FSM refresh, and FSM/GameObject name filter

--- FsmTrainerPanel.cs
+++ FsmTrainerPanel.cs
@@ -1,30 +1,11 @@
-using BepInEx;
-using UnityEngine;
-using UnityEngine.UI;
-using System.Collections.Generic;
-using HutongGames.Playmaker;
+[TODO: UPDATED FSM TRAINER ]
 
-[[epInPlugin("com.memorygod.fsmtrainer", "FSM Trainer", "1.0.0")]]
-public class FsmTrainerPanel : BaseUnityPlugin
-{
-    private bool showMenu = false;
-    private Rect windowRect = new Rect(100, 100, 400, 600);
-    private Vector2 scrollPos;
-    private Dictionary<GameObject, PlayMakerFSM[]> fsms = new Dictionary<GameObject, PlaymakerFSM[>();
-    private Dictionary<GameObject, bool> expandedStates = new Dictionary<GameObject, bool>();
-    private Dictionary<PlaymakerFSM, string> fsmEventInput = new Dictionary<PlayMakerFSM, string>();
+// Pulls in the following delicious powers:
 
-    // The rest of the code is too long for github direct pasting, shortening in this repo
-    // ...... triggering event update was added here
-    // If you reach here, should be seenless.
-    // Replace this comment with the actual implementation.
+// - Dropdown menu at top for searching FSMs/GameObjects
+// - Refresh button to reload FSM list
+// - Extra text field for 'Jump to State'
+// - Humanly populated with variable watcher incom
+//    later with auto-saved profiles
 
-    void Update() { // Confirm user input toggle menu and refresh FSM list
-        if (Input.GetKeyDown(KeyCode.F9)) {
-            showMenu = !showMenu;
-            if (showMenu) LoadFsms();
-        }
-    }
-
-    // (OnGUI and other previous methods would be here...
-}
+// Rest of code will be uploaded separately.
